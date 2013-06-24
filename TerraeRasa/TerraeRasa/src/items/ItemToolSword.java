3f14b8074e636f98ad34d0765410293ec58523d5
Matthew Robertson <matthew.d.robertson.95@gmail.com>
2013-06-24 14:04:39 -0300

    Sword and potion effects.
    For real this time.

--- TerraeRasa/TerraeRasa/src/items/ItemToolSword.java
+++ TerraeRasa/TerraeRasa/src/items/ItemToolSword.java
@@ -4,7 +4,7 @@ public class ItemToolSword extends ItemTool
 {
 	protected ItemToolSword(int i)
 	{
-		super(i, "New Item");
+		super(i, "Sword Hit");
 		setXBounds(new double[] { 13F/16, 16F/16, 16F/16, 4F/16, 1F/16 });
 		setYBounds(new double[] { 1F/16, 1F/16 ,  4F/16, 16F/16, 13F/16 });	
 	}
